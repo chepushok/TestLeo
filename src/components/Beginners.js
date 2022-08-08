2d6f789e2b566ae9324dda76bd37e9addaa28707
badmen87 <dohoiwqel@gmail.com>
2022-08-08 18:53:38 +0300

    huesos

---
+++ src/components/Beginners.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+const Beginners = ()=>{
+
+    let [isVis, setVis] = useState('sidetext-hide')
+    let [bolka, setBolka] = useState(false)
+
+    return (
+        <>
+        <div className='beginners-block center'>
+            <span className='begin-span' onMouseEnter={()=>{setBolka(true);setVis('sidetext')}} onMouseLeave={()=>{setVis('sidetext-hide')}} >BEGINNERS</span>
+        </div>
+        <div className='upper-box' onMouseEnter={()=>bolka?setVis(isVis + ' hover'):setVis('sidetext-hide')} onMouseLeave={()=>{setVis('sidetext-hide');setBolka(false)}}>
+            <span className={isVis}>IDE SETTING</span>
+            <span className={isVis}>SETUP GUIDES</span>
+        </div>
+        <div className='down-box' onMouseEnter={()=>bolka?setVis(isVis + ' hover'):setVis('sidetext-hide')} onMouseLeave={()=>{setVis('sidetext-hide');setBolka(false)}}>
+            <span className={isVis}>SETUP GUIDES</span>
+            <span className={isVis}>IDE SETTING</span>
+        </div>
+        </>
+    )
+}
+export default Beginners
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import '../styles/styles.css'
+import Beginners from './Beginners'
 const Main = () => {
 
     let [isVis, setVis] = useState('sidetext-hide')
@@ -14,19 +15,12 @@ const Main = () => {
                     <div className='newbies-block newbies-pad' >
                         <span>NEWBIES</span>
                     </div>
-                    <div className='beginners-block center'>
-                    <div onMouseEnter={()=>bolka?(setVis('sidetext'), setVisTr('sidetext')):setVis('sidetext-hide')} onMouseLeave={()=>{setVis(('sidetext-hide'),setVisTr('sidetext-hide')); setTimeout(()=>setBolka(false),4000)}}>
-                        <span className={isVisTr}>IDE SETTING</span>
-                        <span className={isVis}>SETUP GUIDES</span>
-                    </div>
-                        <span className='begin-span' onMouseEnter={()=>{setBolka(false);setVis('sidetext');setVisTr('sidetext-1');setBolka(true)}} onMouseLeave={()=>{setVis('sidetext-hide');setVisTr('sidetext-hide')}} >BEGINNERS</span>
-                    <div className='beginners-sidetext' >
-                        <span className={isVis}>SETUP GUIDES</span>
-                        <span className={isVisTr}>IDE SETTING</span>
-                    </div>
-                    </div>
+
+                    <Beginners/>
+
+
                     <div className='advanced-block advanced-pad'>
-                        <span>ADVANCED</span>
+                        <span className='hov'>ADVANCED</span>
                     </div>
                 </div>
             </div>
