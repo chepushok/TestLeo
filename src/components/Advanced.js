c56f64e43d4e0cb49e957f5e3ae69e92aae7f0d9
badmen87 <dohoiwqel@gmail.com>
2022-08-10 01:23:13 +0300

    qwe

--- src/components/Advanced.js
+++ src/components/Advanced.js
@@ -9,14 +9,15 @@ const Newbies = ()=>{
 
     return (
         <>
-        <div className='advanced-block advanced-pad'>
-            <span className={begHoverA} onMouseEnter={()=>{setBolkaA(true);setVisA('sidetext');;setBegHoverA('begin-span-hover')}} onMouseLeave={()=>{setVisA('sidetext-hide');setBegHoverA('advanced-span')}} >ADVANCED</span>
-        </div>
-        <div className='upper-boxN' onMouseEnter={()=>bolkaA?(setVisA(isVisA + ' hover'),setBegHoverA('begin-span-hover')):setVisA('sidetext-hide')} onMouseLeave={()=>{setVisA('sidetext-hide');setBolkaA(false);setBegHoverA('begin-span')}}>
+
+        <div className='upper-boxA' onMouseEnter={()=>bolkaA?(setVisA(isVisA + ' hover'),setBegHoverA('begin-span-hover')):setVisA('sidetext-hide')} onMouseLeave={()=>{setVisA('sidetext-hide');setBolkaA(false);setBegHoverA('begin-span')}}>
             <span className={isVisA}>IDE SETTING</span>
             <span className={isVisA}>SETUP GUIDES</span>
         </div>
-        <div className='down-boxN' onMouseEnter={()=>bolkaA?setVisA((isVisA + ' hover'),setBegHoverA('begin-span-hover')):setVisA('sidetext-hide')} onMouseLeave={()=>{setVisA('sidetext-hide');setBolkaA(false);setBegHoverA('begin-span')}}>
+        <div className='advanced-block advanced-pad'>
+            <span className={begHoverA} onMouseEnter={()=>{setBolkaA(true);setVisA('sidetext');;setBegHoverA('begin-span-hover')}} onMouseLeave={()=>{setVisA('sidetext-hide');setBegHoverA('advanced-span')}} >ADVANCED</span>
+        </div>
+        <div className='down-boxA' onMouseEnter={()=>bolkaA?setVisA((isVisA + ' hover'),setBegHoverA('begin-span-hover')):setVisA('sidetext-hide')} onMouseLeave={()=>{setVisA('sidetext-hide');setBolkaA(false);setBegHoverA('begin-span')}}>
             <span className={isVisA}>SETUP GUIDES</span>
             <span className={isVisA}>IDE SETTING</span>
         </div>
