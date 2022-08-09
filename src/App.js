6534bbce6ef8b143f0f3b5a9c3ab17183981e9d0
badmen87 <dohoiwqel@gmail.com>
2022-08-09 18:34:21 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Main from './components/Main'
-import BlogNewb from './components/BlogNewb'
+import BlogNewN from './components/BlogNewN'
+import BlogNewB from './components/BlogNewB'
+import BlogNewA from './components/BlogNewA'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -11,8 +13,10 @@ function App() {
 
       <Routes>
 
-        <Route path='/' element={<Main/>}/>  
-        <Route path='/beginners' element={<BlogNewb></BlogNewb>}/>
+        <Route path='/' element={<Main/>}/> 
+        <Route path='/Newbies' element={<BlogNewN/>}/> 
+        <Route path='/Beginners' element={<BlogNewB/>}/>
+        <Route path='/Advanced' element={<BlogNewA/>}/>
       </Routes>
 
     </Router>
---
+++ src/components/BlogNewA.js
@@ -0,0 +1,29 @@
+import '../styles/blog-style.css';
+import React, { useState } from 'react';
+import {Post} from './Post'
+import {PostRU} from './PostRU'
+
+function BlogNewb() {
+    const[choose,setChoose] = useState('choose')
+    const[post,setPost] = useState(<><Post/><Post/><Post/></>)
+  
+    return (
+    <div className='Blog'>
+      <div className="App">
+        <div className='logo'>
+          <div className='buttons'>
+            <div className={choose} onClick={()=>setChoose('choose level')}>Newbies</div> <div className='choose'>Beginers</div> <div className='choose'>Advanced</div>
+          </div>
+        </div>
+        <div className='Posts'>
+          <div className='langbuttons'>
+            <button onClick={()=> setPost(<><PostRU/><PostRU/><PostRU/></>)}>RU</button>
+            <button onClick={()=> setPost(<><Post/><Post/><Post/></>)}>EN</button>
+          </div>
+        {post}
+        </div>
+      </div>
+    </div>
+    );
+  }
+  export default BlogNewb;
