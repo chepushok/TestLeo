be28e7e05afe6e5495f374ee29623a48a9a07e54
badmen87 <dohoiwqel@gmail.com>
2022-08-11 19:31:31 +0300

    asd

--- src/components/Beginners.js
+++ src/components/Beginners.js
@@ -14,8 +14,10 @@ const Beginners = ()=>{
                     <span className={isVisB}>SETUP GUIDES</span>
                 </div>
             </div>
-            <div className='beginners-block center'>
-                <Link to ='/Beginners'> <span className={begHover} onMouseEnter={()=>{setBolkaB(true);setVisB('sidetext');setBegHover('begin-span-hover')}} onMouseLeave={()=>{setVisB('sidetext-hide');setBegHover('begin-span')}} >BEGINNERS</span> </Link>
+            <div className='wrap'>
+                <div className='beginners-block center'>
+                    <Link to ='/Beginners'> <span className={begHover} onMouseEnter={()=>{setBolkaB(true);setVisB('sidetext');setBegHover('begin-span-hover')}} onMouseLeave={()=>{setVisB('sidetext-hide');setBegHover('begin-span')}} >BEGINNERS</span> </Link>
+                </div>
             </div>
             <div className='wrapper'>
                 <div className='down-box' onMouseEnter={()=>bolkaB?setVisB((isVisB + ' hover'),setBegHover('begin-span-hover')):setVisB('sidetext-hide')} onMouseLeave={()=>{setVisB('sidetext-hide');setBolkaB(false);setBegHover('begin-span')}}>
